cdfe4e461e57fdcd611769d82d6c4f9f62e8b661
PetiteSirene <ariel.lissak@gmail.com>
2022-07-12 16:23:44 +0200

    Screen rejoindre/creer coloc

--- screens/NoColoc.tsx
+++ screens/NoColoc.tsx
@@ -1,11 +1,56 @@
 import React, { useEffect, useState } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity, TextInput} from 'react-native'
+import TopBackNavigation from '../components/TopBackNavigation'
+import * as Haptics from 'expo-haptics';
+
 
 const NoColocScreen = ()  => {
+    const [nomColoc, onChangeNom] = React.useState(null);
+    const [codeColoc, onChangeCode] = React.useState(null);
+    
     return(
+
+
+        
+        
         <View style={styles.container}>
-            <Text>Goddamn</Text>
+            <SafeAreaView>
+                <TopBackNavigation/>
+            </SafeAreaView>
+            
+            <View style={styles.creerColocContainer}>
+                <Text style={styles.texteBlanc}>Créer une coloc</Text>
+                <TextInput
+                style={styles.inputBlanc}
+                onChangeText={onChangeNom}
+                value={nomColoc}
+                placeholder="Choisir un nom pour la coloc"
+                placeholderTextColor='lightgrey'
+                />
+                <View style={styles.ButtonBlanc}>
+                    <TouchableOpacity onPress={() => { Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium)}}>
+                    <Text style={styles.texteCreer}>Créer</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+
+            <View style={styles.rejoindreColocContainer}>
+                <Text style={styles.texteNoir}>Rejoindre une coloc</Text>
+                <TextInput
+                style={styles.inputGris}
+                onChangeText={onChangeCode}
+                value={codeColoc}
+                placeholder="Entre un code de coloc"
+                placeholderTextColor='gray'
+                />
+                <View style={styles.ButtonBleu}>
+                    <TouchableOpacity onPress={() => { Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium)}}>
+                    <Text style={styles.texteRejoindre}>Rejoindre</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
         </View>
+        
     )
 }
 
@@ -14,10 +59,92 @@ const NoColocScreen = ()  => {
 const styles = StyleSheet.create({
 
     container: {
-        paddingLeft: 16,
-        paddingRight: 16,
         flex: 1,
-        backgroundColor:'#EDF0FA'
-    }})
+        backgroundColor:'#EDF0FA',
+        flexDirection:'column',
+        padding:25
+        },
+    creerColocContainer:{
+        marginBottom:15,
+        marginTop:15,
+        backgroundColor:'#172ACE',
+        flex:0.5,
+        borderRadius:15,
+        flexDirection:'column',
+        justifyContent:'center',
+        alignItems:'center',
+        elevation:5,
+        shadowColor: 'black',
+        shadowOffset: {width: -2, height: 1},
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+    },
+    rejoindreColocContainer:{
+        marginBottom:15,
+        marginTop:15,
+        backgroundColor:'white',
+        flex:0.5,
+        borderRadius:15,
+        flexDirection:'column',
+        justifyContent:'center',
+        alignItems:'center',
+        elevation:2,
+        shadowColor: 'black',
+        shadowOffset: {width: -2, height: 1},
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+    },
+    inputBlanc: {
+        height: 44,
+        marginTop: 13,
+        padding: 10,
+        borderRadius: 5,
+        backgroundColor: 'white'
+    },
+    inputGris: {
+        height: 44,
+        marginTop: 13,
+        padding: 10,
+        borderRadius: 5,
+        backgroundColor: '#E8E8E8'
+    },
+    texteBlanc:{
+        color:'white',
+        fontSize:30
+    },
+    texteNoir:{
+        color:'black',
+        fontSize:30
+    },
+    texteCreer: {
+        fontSize: 16,
+        fontWeight: '600',
+        color: 'black',
+        textAlign: 'center',
+      },
+      texteRejoindre: {
+        fontSize: 16,
+        fontWeight: '600',
+        color: 'white',
+        textAlign: 'center',
+      },
+    ButtonBlanc: {
+        marginTop:50,
+        height: 40,
+        borderRadius: 5,
+        backgroundColor: 'white',
+        width: 154,
+        justifyContent: 'center',
+    },
+    ButtonBleu: {
+        marginTop:50,
+        height: 40,
+        borderRadius: 5,
+        backgroundColor: '#172ACE',
+        width: 154,
+        justifyContent: 'center',
+    }
+
+})
 export default NoColocScreen;
 
