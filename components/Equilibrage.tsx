4293e263953b19b0021d57c01756f8366d1f43bc
Paul MAZEAU <mazeau.paul@gmail.com>
2022-07-13 21:40:43 +0200

    on continue l'equilibrage

--- components/Equilibrage.tsx
+++ components/Equilibrage.tsx
@@ -1,22 +1,67 @@
-import React from 'react'
-import { BarChart, Grid } from 'react-native-svg-charts'
-import {View, Text, StyleSheet, TouchableOpacity, Image} from 'react-native';
+import React from "react";
+import {ProgressChart} from "react-native-chart-kit";
+import { Dimensions, View } from "react-native";
+import { Bar, VictoryAxis, VictoryBar, VictoryChart, VictoryStack } from "victory-native";
+
+
+const solde=[
+  { x: 'paul', y: 2 },
+  { x: 'pablo', y: 3 },
+  { x: 'tomy', y: -5 },
+  { x: 'kev', y: 4 }
+];
+
+
+class Equilibrage extends React.Component {
  
-const Equilibrage = () => {
 
-  const fill = '#172ACE'
-  const data = [50, -4, 40, 95, -24, 10]
 
-    return(     
-            <BarChart 
-            style={{ height: 200 }} 
-            data={data} 
-            svg={{ fill }} 
-            horizontal={true}
-            >
-            {/* <Image source={require(../Img/avatar1.png)} /> */}
-            </BarChart>
-        )
-}
+    
+  render() {
+
+    return (
+
+      <View style={{
+        backgroundColor: 'white', 
+        borderRadius: 13, 
+        width: 'auto', 
+        height: 300,
+        marginTop: 15,
+        marginBottom: 15
+      }}>
+      
+        <VictoryChart height={350} width={350}
+          domainPadding={{ x: 10, y: 10 }}
+          scale={{ x: "time" }}
+          horizontal={true}        
+        >
+           <VictoryAxis dependentAxis crossAxis 
+           standalone={true}
+           style={{ 
+            axis: {stroke: "transparent"}, 
+            ticks: {stroke: "transparent"},
+            tickLabels: { fill:"transparent"} 
+           }}/>
+
+           <VictoryAxis crossAxis
+           style={{ 
+            axis: {stroke: "transparent"}, 
+            ticks: {stroke: "transparent"},
+           }}
+           />
+          
+          <VictoryBar
+            dataComponent={ <Bar cornerRadius={4}/> }
+            style={{data: {
+              fill: ({ datum }) => datum.x === 1 ? "#000000" : "#172ACE",
+            },}}
+            data={solde}
+          />
+          
+        </VictoryChart>
+        </View>
+    );
+  }
+ }
 
 export default Equilibrage
