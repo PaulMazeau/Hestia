256dee16d74a4d2fedab175dfaaae52363fff701
Alexandre Wilk <alex@MacBook-Air-de-Alexandre.local>
2022-07-20 09:43:13 +0200

    damn

--- Context/userContextFile.tsx
+++ Context/userContextFile.tsx
@@ -3,3 +3,4 @@ import { createContext } from 'react';
 
 //provide le contexte a tous les screens
 export  const UserContext = createContext(null);
+export const UserListContext = createContext(null);
